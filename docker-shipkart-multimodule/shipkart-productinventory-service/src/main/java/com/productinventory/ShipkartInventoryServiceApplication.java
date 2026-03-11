178dd68df75ee587041402844badad1cc28c3cbb
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-11 15:14:00 +0530

    Remove unused import

--- docker-shipkart-multimodule/shipkart-productinventory-service/src/main/java/com/productinventory/ShipkartInventoryServiceApplication.java
+++ docker-shipkart-multimodule/shipkart-productinventory-service/src/main/java/com/productinventory/ShipkartInventoryServiceApplication.java
@@ -2,7 +2,6 @@ package com.productinventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ShipkartInventoryServiceApplication {
