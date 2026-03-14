7fbbdb772376459b7e038aa3343b3ed2a8f45ad3
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-14 16:50:52 +0530

    Scaffolding for basic kafka demo purpose - multi-module project
    Has one parent project with two module projects - for consumer & for
    producer application.

---
+++ zkafka-basics-multimodule/zkafka-basics-producer/src/main/java/com/example/ZkafkaBasicsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkafkaBasicsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkafkaBasicsProducerApplication.class, args);
+	}
+
+}
