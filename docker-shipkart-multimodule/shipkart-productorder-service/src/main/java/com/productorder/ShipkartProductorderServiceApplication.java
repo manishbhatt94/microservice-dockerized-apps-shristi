51e2baebd7370a6af6c9045543ec807751a6974a
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-21 12:41:31 +0530

    Scaffolding for "order" microservice

---
+++ docker-shipkart-multimodule/shipkart-productorder-service/src/main/java/com/productorder/ShipkartProductorderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartProductorderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartProductorderServiceApplication.class, args);
+	}
+
+}
---
+++ docker-shipkart-multimodule/shipkart-productorder-service/src/test/java/com/productorder/ShipkartProductorderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipkartProductorderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
