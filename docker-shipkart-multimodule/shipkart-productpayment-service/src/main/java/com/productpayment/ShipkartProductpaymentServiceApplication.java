96560a4f96681ca8dbd3f894aa6a2dcb170484a7
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-23 18:05:16 +0530

    Scaffolding for product-payment app

---
+++ docker-shipkart-multimodule/shipkart-productpayment-service/src/main/java/com/productpayment/ShipkartProductpaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartProductpaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartProductpaymentServiceApplication.class, args);
+	}
+
+}
---
+++ docker-shipkart-multimodule/shipkart-productpayment-service/src/test/java/com/productpayment/ShipkartProductpaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipkartProductpaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
