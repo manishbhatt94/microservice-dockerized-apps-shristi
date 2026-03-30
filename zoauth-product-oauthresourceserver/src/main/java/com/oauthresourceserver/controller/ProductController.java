ab358141bee109bc5a68c2871db89a83524c720a
Manish Bhatt <manishbhatt594@gmail.com>
2026-03-30 18:28:21 +0530

    Add controller & model in resource server

---
+++ zoauth-product-oauthresourceserver/src/main/java/com/oauthresourceserver/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.oauthresourceserver.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oauthresourceserver.model.Product;
+
+@RestController
+public class ProductController {
+
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> getAll() {
+		return ResponseEntity.ok(fetchProducts());
+	}
+
+	@GetMapping("/products/productId/{productId}")
+	public ResponseEntity<Product> getById(@PathVariable int productId) {
+		Product matched = fetchProducts().stream().filter(product -> product.getProductId().equals(productId))
+				.findFirst().orElseThrow();
+		return ResponseEntity.ok(matched);
+	}
+
+	private List<Product> fetchProducts() {
+		return Arrays.asList(new Product(1, "Nike Jordans Relax Sneakers", 15999.0),
+				new Product(2, "Skmei Digital Sports Wristwatch", 2520.0),
+				new Product(3, "Ikea Spring Workdesk Standing Desk", 19500.0),
+				new Product(4, "Yamaha YamaLube Chain Lube", 399.0));
+	}
+
+}
---
+++ zoauth-product-oauthresourceserver/src/main/java/com/oauthresourceserver/model/Product.java
@@ -0,0 +1,18 @@
+package com.oauthresourceserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+	Integer productId;
+
+	String productName;
+
+	double price;
+
+}
