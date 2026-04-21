bdfc407c5133120f4fe350ce50c5d92d880120e8
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-21 17:35:15 +0530

    Update security Configuration for oauth-auth-server app

--- zoauth-authorization-server/src/main/java/com/authserver/config/AuthorizationServerConfig.java
+++ zoauth-authorization-server/src/main/java/com/authserver/config/AuthorizationServerConfig.java
@@ -1,10 +1,52 @@
 package com.authserver.config;
 
+import java.util.UUID;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+		http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+				.with(authorizationServerConfigurer, Customizer.withDefaults())
+				.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+		return http.build();
+	}
+
+	@Bean
+	RegisteredClientRepository clientRegistrationRepository() {
+		return new InMemoryRegisteredClientRepository(clientRegistration());
+	}
+
+	@Bean
+	PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	private RegisteredClient clientRegistration() {
+		RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString()).clientId("order-client")
+				.clientSecret(encoder().encode("order-client-secret"))
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS).scope("read").build();
+		System.out.println("AuthorizationServerConfig::clientRegistration - RegisteredClient = " + client);
+		return client;
+	}
 
 }
